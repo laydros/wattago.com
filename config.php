725d07b25178a151397f9f8d08c01524509a35c8
Jason Hamilton <laydros@gmail.com>
2012-05-23 02:11:59 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'TEBONN1COJN0ngG9g1wg');
-define('CONSUMER_SECRET', 'VMUEN3YR9wp6BIKmWfP3srJVm6XbkmA5B1fj7AN7M');
+define('CONSUMER_KEY', '????');
+define('CONSUMER_SECRET', '????');
 define('OAUTH_CALLBACK', 'http://laydros.freeshell.org/twitter/callback.php');
